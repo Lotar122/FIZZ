7bda159547eab61dadf7b2f28743ceaf7bee1b93
Lotar122 <dajlizaka9@gmail.com>
2025-10-05 20:32:22 +0200

    Fixed C/C++ mixup

--- src/Functions/InitializeTimerForServos/initializeTimerForServos.hpp
+++ src/Functions/InitializeTimerForServos/initializeTimerForServos.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include "types.h"
-
 extern "C" {
-#include "stm32l476xx.h"
+#include "types.h"
 }
 
 void initializeTimer3ForServos(uint16_t initialStateS1 = 1500, uint16_t initialStateS2 = 1500, uint16_t initialStateS3 = 1500, uint16_t initialStateS4 = 1500);
